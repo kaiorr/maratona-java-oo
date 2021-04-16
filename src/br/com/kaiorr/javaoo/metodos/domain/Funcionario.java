3cc6c18bb70b2d49bc43252fcb819804a6679786
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-04-16 20:06:34 -0300

    Resolição exercício salário

--- src/br/com/kaiorr/javaoo/metodos/domain/Funcionario.java
+++ src/br/com/kaiorr/javaoo/metodos/domain/Funcionario.java
@@ -8,17 +8,27 @@ public class Funcionario {
     public void imprimeFuncionario() {
         System.out.println(this.nome);
         System.out.println(this.idade);
+        if (salarios == null) {
+            System.out.println("É obrigatório informar valor de salário");
+            return;
+        }
         for(double salario: salarios) {
             System.out.print(salario + "");
         }
+        mediaSalario();
     }
 
     public void mediaSalario() {
+        if(salarios == null) {
+            System.out.println("É obrigatório informar valor de salário");
+            return;
+        }
         double media = 0;
         for(double salario: salarios) {
             media += salario;
         }
         media /= salarios.length;
+        System.out.println("\nMedia Salarial R$" + media);
     }
 }
 
